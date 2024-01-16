ab6fc8fee2fa462d712765555008fca4c86dc186
sinkovit <sinkovit@sdsc.edu>
2024-01-16 11:58:52 -0800

    Adds history example

---
+++ history/history.sh
@@ -0,0 +1,9 @@
+# This script contains commands for exploring command line history. It
+# contains a few commands that may not have been covered yet but their
+# purpose should be somewhat obvious.
+
+# Show history
+echo "Executing history"
+history
+echo -e "\nEnter return to continue"
+read
